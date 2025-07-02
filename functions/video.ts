679e79978a7ae182f735a3d65bb3a1c629e058be
ericzhang.design <hello@ericzhang.design>
2025-07-02 14:35:39 +1000

    Update video.ts

--- functions/video.ts
+++ functions/video.ts
@@ -1,17 +1,12 @@
 export const onRequest: PagesFunction = async ({ request }) => {
-  const url = new URL(request.url);
-  const response = await fetch(request);
-  if (url.pathname.startsWith("/video/")) {
-    const newHeaders = new Headers(response.headers);
-    newHeaders.set(
-      "Cache-Control",
-      "public, max-age=31536000, immutable"
-    );
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: newHeaders,
-    });
-  }
-  return response;
-};
+  const url = new URL(request.url)
+  const videoPath = url.pathname
+
+  // 读取视频文件
+  const res = await fetch(`https://raw.githubusercontent.com/yancheng-zhang/media/main${videoPath}`)
+
+  // 透传响应体和 Content-Type
+  const headers = new Headers(res.headers)
+  headers.set("cache-control", "public, max-age=31536000, immutable")
+  return new Response(res.body, { headers })
+}
