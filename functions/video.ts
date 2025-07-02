01b1048ca1abc94b8539752251c878d895701604
ericzhang.design <hello@ericzhang.design>
2025-07-02 14:32:22 +1000

    Update video.ts

--- functions/video.ts
+++ functions/video.ts
@@ -1,14 +1,17 @@
-export const onRequest: PagesFunction = async (context) => {
-    const { pathname } = new URL(context.request.url)
-    if (
-        pathname.endsWith(".mp4") ||
-        pathname.endsWith(".webm") ||
-        pathname.endsWith(".mov")
-    ) {
-        const res = await context.next()
-        const headers = new Headers(res.headers)
-        headers.set("cache-control", "public, max-age=31536000, immutable")
-        return new Response(res.body, { ...res, headers })
-    }
-    return await context.next()
-}
+export const onRequest: PagesFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const response = await fetch(request);
+  if (url.pathname.startsWith("/video/")) {
+    const newHeaders = new Headers(response.headers);
+    newHeaders.set(
+      "Cache-Control",
+      "public, max-age=31536000, immutable"
+    );
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders,
+    });
+  }
+  return response;
+};
